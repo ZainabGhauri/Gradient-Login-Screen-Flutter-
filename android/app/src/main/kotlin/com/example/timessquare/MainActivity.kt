a554cbc443a07253bcf03ea48f33fd031a663280
zainab <zainabghauri7799@gmail.com>
2023-04-03 08:59:37 -0700

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/timessquare/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.timessquare
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Colors/color.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const Color mainBlue = Color(0xff1d075c);
+const Color mainPurple = Color(0xff8618c3);
